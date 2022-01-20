048fc1fb241cf485cb43bed96e47db311fd51ef5
VitaliMalyavko <95131243+VitaliMalyavko@users.noreply.github.com>
2022-01-21 01:44:59 +0300

    firts commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+new Swiper ('.image-slider',{
+    navigation: {
+        nextE1: '.swiper-button-next',
+        prevE1: '.swiper-button-prev'
+    },
+    pagination:{
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+});
